1105635eba8fcb009702dac1e7648b8c5dd57c9a
Alexander Trakhimenok <a_trakhimenok@dellteam.com>
2020-10-06 21:52:01 +0100

    Migrated to Go runtime 1.11

--- gae_root/main.go
+++ gae_root/main.go
@@ -1,12 +1,14 @@
-package appengine
+package main
 
 import (
 	"bitbucket.org/asterus/debtstracker-server/gae_app"
 	"github.com/strongo/bots-framework/hosts/appengine"
 	"github.com/strongo/log"
+	"google.golang.org/appengine"
 )
 
-func init() {
+func main() {
 	log.AddLogger(gaehost.GaeLogger)
 	gaeapp.Init(gaehost.GaeBotHost{})
+	appengine.Main()
 }
