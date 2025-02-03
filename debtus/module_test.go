0023134efeec094879cfcb31f9e247b906c68acb
Alexander Trakhimenok <alex@trakhimenok.com>
2025-02-03 08:18:53 +0000

    fix: moved api routing initialization from sneat-go - tests fixed

--- debtus/module_test.go
+++ debtus/module_test.go
@@ -10,7 +10,7 @@ func TestModule(t *testing.T) {
 	m := Module()
 	module.AssertModule(t, m, module.Expected{
 		ModuleID:      const4debtus.ModuleID,
-		HandlersCount: 0,
+		HandlersCount: 4,
 		DelayersCount: 2,
 	})
 }
